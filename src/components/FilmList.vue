053df7d3604a4be5b6399ab6e6cdf2adfe9fd2f3
Louis <ls.bouet@gmail.com>
2022-06-02 11:58:15 +0200

    add film list

---
+++ src/components/FilmList.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name:'FilmList',
+    props: ['filmList']
+}
+
+</script>
+
+<template>
+    <ul>
+        <li v-for="film in filmList" :key="film.title">
+            {{ film.title }}
+        </li>
+    </ul>
+
+</template>
--- src/components/FormContact.vue
+++ src/components/FormContact.vue
@@ -1,6 +1,43 @@
 <template>
-    <form>
-        <input type="text">
+    <form @submit.prevent="handleSubmit">
+        <input type="text" placeholder="Le titre du film" v-model="credentials.title">
+        <input type="text" placeholder="Durée du film" v-model="credentials.duration">
+        <input type="text" placeholder="Notation du film" v-model="credentials.notation">
         <input type="submit" value="Envoyer">
     </form>
+
+    <FilmList :filmList="filmList"/>
 </template>
+
+
+<script>
+import FilmList from '@/components/FilmList.vue'
+export default {
+    name: 'FormContact',
+    components: {
+        FilmList
+    },
+    data() {
+        return {
+            credentials: {
+                title: '',
+                duration: '',
+                notation: ''
+            },
+            filmList: []
+        }
+    },
+    methods: {
+        handleSubmit() {
+            this.filmList.push(this.credentials)
+            this.credentials = {
+                title: '',
+                duration: '',
+                notation: ''
+            }
+            console.log(this.filmList);
+        }
+    }
+
+}
+</script>
