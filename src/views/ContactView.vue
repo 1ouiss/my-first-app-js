44eb5e487770d824392c68b5f4e0713301453bfb
Louis <ls.bouet@gmail.com>
2022-06-02 11:08:06 +0200

    first commit

---
+++ src/views/ContactView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1> {{ message }}</h1>
+        <FormContact />
+    </div>
+</template>
+
+<script>
+    import FormContact from '../components/FormContact.vue';
+    export default {
+        name: 'ContactView',
+        components: {
+            FormContact
+        },
+        data() {
+            return {
+                message: 'Contact page'
+            }
+        }
+    }
+</script>
